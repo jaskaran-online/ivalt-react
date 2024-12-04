15ef2444760bdc4ce693314e42109530369d1baa
Jaskaran Singh <jaskaran.singh@ivalt.com>
2024-12-04 16:24:55 +0530

    Fix issues

--- example-usage.tsx
+++ example-usage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { initializeIValt } from "ivalt-react";
-import { BiometricAuthForm } from "./dist/components/BiometricAuthForm";
+import { BiometricAuthForm } from "ivalt-react/dist/components/BiometricAuthForm";
 
 // Initialize the package with your configuration
 initializeIValt({
